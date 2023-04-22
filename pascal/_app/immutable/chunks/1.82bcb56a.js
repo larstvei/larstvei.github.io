57db2953bf3e8604575d931b8b4f9676360775a6
larstvei <larstvei@ifi.uio.no>
2023-04-22 19:00:23 +0200

    Try again
    Don't think it will work...

--- pascal/_app/immutable/chunks/1.82bcb56a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.d67bd47f.js";export{t as component};
---
+++ pascal/_app/immutable/chunks/2.1c9caf25.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.886b7f02.js";export{t as component};
